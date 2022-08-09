fd989f5c9c07a3b5ea917b3f87ae17cf1c8941ea
Alexej Melnyk <98652436+Alexej-AM@users.noreply.github.com>
2022-08-09 23:52:25 +0300

    1+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
 
+
+buttonStart.addEventListener('click', onButtonClickStart);
+buttonStop.addEventListener('click', onButtonClickStop);
+let timerId = null;
+
+
+
+function onButtonClickStart(event) { 
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+timerId = setInterval(() => {
+    const changeColor =  bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    
+ };
+
+function onButtonClickStop(event) { 
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    
+    clearInterval(timerId);
+ };
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ };
