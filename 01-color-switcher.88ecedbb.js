7a55a55ec53272323df689eaca0f076e09598469
Alexej-AM <98652436+Alexej-AM@users.noreply.github.com>
2022-08-09 20:52:55 +0000

    Deploying to gh-pages from @ Alexej-AM/goit-js-hw-09@fd989f5c9c07a3b5ea917b3f87ae17cf1c8941ea 🚀

---
+++ 01-color-switcher.88ecedbb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(a){t.disabled=!0,e.disabled=!1,o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(n){t.disabled=!1,e.disabled=!0,clearInterval(o)}));var o=null}();
+//# sourceMappingURL=01-color-switcher.88ecedbb.js.map
